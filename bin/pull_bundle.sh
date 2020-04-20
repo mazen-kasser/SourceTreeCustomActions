ff84f5566f82f2517ed03acf4df8758364fc8fa6
Mazen Kasser <mazen.kasser@anz.com>
2020-04-20 13:43:36 +1200

    Fix bug that prevented from pulling master from remote/origin

--- bin/pull_bundle.sh
+++ bin/pull_bundle.sh
@@ -4,7 +4,7 @@ bundle_name=$1
 # replace _ with / to create gitflow path
 branch_name=${bundle_name/_//}
 
-git fetch $bundle_name $branch_name:$branch_name
+git fetch $bundle_name $branch_name:remotes/origin/$branch_name
 git checkout $branch_name
 
 echo "Bundle: $bundle_name"
