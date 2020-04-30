d627f9ea866b76734048be3b1889c73491e4a5ce
Mazen Kasser <mazen.kasser@anz.com>
2020-04-30 18:34:33 +1200

    Fix a bug around detecting if the bundle to pull is master vs other branch

--- bin/pull_bundle.sh
+++ bin/pull_bundle.sh
@@ -4,7 +4,12 @@ bundle_name=$1
 # replace _ with / to create gitflow path
 branch_name=${bundle_name/_//}
 
-git pull $bundle_name $branch_name:remotes/origin/$branch_name
+if [ $branch_name == "master" ]; then
+    git pull $bundle_name $branch_name:remotes/origin/$branch_name
+else
+    git fetch $bundle_name $branch_name
+fi
+
 git checkout $branch_name
 
 echo "Bundle: $bundle_name"
